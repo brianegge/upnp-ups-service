e85f4bbfac96239b49b045209f3666c0673d7eb3
Brian Egge <brianegge@gmail.com>
2018-12-19 11:20:47 -0500

    Add Cyberpower tests

--- lib/ups.py
+++ lib/ups.py
@@ -2,7 +2,7 @@ import subprocess
 import re
 
 def check_ups():
-    # output = subprocess.check_output(["/bin/cat", "test/apc.txt"])
+    # output = subprocess.check_output(["/bin/cat", "test/cyberpower-off.txt"])
     output = subprocess.check_output(["/bin/upsc", "myups"])
     result = {}
     for line in output.splitlines():
